85b889d59e60f4301853654fc932f787dbaf1a69
Tanishka Gupta <tanishkagupta2207@gmail.com>
2024-04-09 03:06:54 +0530

    login update

--- controllers/login.js
+++ controllers/login.js
@@ -2,10 +2,11 @@ import User from '../models/user.js';
 
 const loginUser = async (req, res) => {
     const {emailId, password } = req.body;
+    console.log(emailId,password);
     try {
         const existingUser = await User.findOne({ "emailId":emailId, "password":password, "status":"activated" });
         if (existingUser) {
-            return res.json({ message: 'Logged in successfully' });
+            return res.json({ message: 'Logged in successfully', userId: existingUser._id });
         }
         else{
             return res.json({ message: 'Incorrect email-id or password' });
