3f649a5145406ac82739ad362111067316f623b2
Tanishka Gupta <tanishkagupta2207@gmail.com>
2024-04-09 22:34:29 +0530

    form1 model

---
+++ models/form1.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    firstName: { 
+        type: String, 
+        required: true 
+    },
+    lastName: { 
+        type: String, 
+        required: true 
+    },
+    emailId: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['activated', 'pending']
+    }
+});
+
+const user = mongoose.model('user', userSchema);
+
+export default user;
